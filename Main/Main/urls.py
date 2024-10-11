b5a59053ff2004df87b465a9193424290441430e
Viktor <ccowes050807@gmail.com>
2024-10-12 01:16:53 +0300

    Home

--- Main/Main/urls.py
+++ Main/Main/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Home.views import render_home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', render_home ),
 ]
